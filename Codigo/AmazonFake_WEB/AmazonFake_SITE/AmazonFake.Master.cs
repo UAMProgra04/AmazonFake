ca1957419043f0ce7b6f2661c85214737eec0e7f
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-11-24 16:09:19 -0600

    Update_24112018_1607
    Modificacion de toda la plantilla, por la propuesta por Andrey.
    Se logra integrar la plantilla en html. inicia proceso de migrado de etiquetas HTML a  ASP y reconstrucción de codigo ya establecido en el modelo anterior

--- Codigo/AmazonFake_WEB/AmazonFake_SITE/AmazonFake.Master.cs
+++ Codigo/AmazonFake_WEB/AmazonFake_SITE/AmazonFake.Master.cs
@@ -12,86 +12,86 @@ namespace AmazonFake_SITE
         #region Administracion_Perfiles
         private void AdministracionPerfiles()
         {
-            #region Opciones Perfil de usuario
-            this.opcNombreUsuario.Visible = false;
-            this.opcNombreUsuario_Perfil.Visible = false;
-            this.opcNombreUsuario_Payment.Visible = false;
-            this.opcNombreUsuario_PerfilAdministrativo.Visible = false;
-            #endregion
+            //#region Opciones Perfil de usuario
+            //this.opcNombreUsuario.Visible = false;
+            //this.opcNombreUsuario_Perfil.Visible = false;
+            //this.opcNombreUsuario_Payment.Visible = false;
+            //this.opcNombreUsuario_PerfilAdministrativo.Visible = false;
+            //#endregion
 
-            this.opcIndex.Visible = true;
-            this.opcDepartamentos.Visible = false;
+            //this.opcIndex.Visible = true;
+            //this.opcDepartamentos.Visible = false;
 
-            #region Opciones Administrativas
-            this.opcAdministrativa.Visible = false;
-            this.opcAdministrativa_Clientes.Visible = false;
-            this.opcAdministrativa_Usuarios.Visible = false;
-            this.opcAdministrativa_Productos.Visible = false;
-            #endregion
+            //#region Opciones Administrativas
+            //this.opcAdministrativa.Visible = false;
+            //this.opcAdministrativa_Clientes.Visible = false;
+            //this.opcAdministrativa_Usuarios.Visible = false;
+            //this.opcAdministrativa_Productos.Visible = false;
+            //#endregion
 
-            this.opcCarrito.Visible = false;
+            //this.opcCarrito.Visible = false;
 
-            #region Opcones de Login
-            this.opcLogin.Visible = true;
-            this.opcCerrarSesion.Visible = false;
-            #endregion
+            //#region Opcones de Login
+            //this.opcLogin.Visible = true;
+            //this.opcCerrarSesion.Visible = false;
+            //#endregion
 
-            if (Session["UsuarioSession"] != null)
-            {
-                if (Session["UsuarioSession"].ToString().Equals("Test@hotmail.com"))
-                {
-                    string NombreUsuario = Session["UsuarioSession"].ToString().ToUpper();
-                    lbl_NombreUsuario.Text = "Bienvenido " + NombreUsuario;
+            //if (Session["UsuarioSession"] != null)
+            //{
+            //    if (Session["UsuarioSession"].ToString().Equals("Test@hotmail.com"))
+            //    {
+            //        string NombreUsuario = Session["UsuarioSession"].ToString().ToUpper();
+            //        lbl_NombreUsuario.Text = "Bienvenido " + NombreUsuario;
 
-                    //Opciones Perfil de usuario
-                    this.opcNombreUsuario.Visible = true;
-                    this.opcNombreUsuario_Perfil.Visible = true;
-                    this.opcNombreUsuario_Payment.Visible = true;
-                    this.opcNombreUsuario_PerfilAdministrativo.Visible = false;
+            //        //Opciones Perfil de usuario
+            //        this.opcNombreUsuario.Visible = true;
+            //        this.opcNombreUsuario_Perfil.Visible = true;
+            //        this.opcNombreUsuario_Payment.Visible = true;
+            //        this.opcNombreUsuario_PerfilAdministrativo.Visible = false;
 
-                    this.opcIndex.Visible = true;
-                    this.opcDepartamentos.Visible = true;
-                    this.opcAdministrativa.Visible = false;
-                    this.opcCarrito.Visible = true;
-                    this.opcLogin.Visible = false;
-                    this.opcCerrarSesion.Visible = true;
-                }
-                if (Session["UsuarioSession"].ToString().Equals("Test2@hotmail.com"))
-                {
-                    string NombreUsuario = Session["UsuarioSession"].ToString().ToUpper();
-                    lbl_NombreUsuario.Text = "Bienvenido " + NombreUsuario;
+            //        this.opcIndex.Visible = true;
+            //        this.opcDepartamentos.Visible = true;
+            //        this.opcAdministrativa.Visible = false;
+            //        this.opcCarrito.Visible = true;
+            //        this.opcLogin.Visible = false;
+            //        this.opcCerrarSesion.Visible = true;
+            //    }
+            //    if (Session["UsuarioSession"].ToString().Equals("Test2@hotmail.com"))
+            //    {
+            //        string NombreUsuario = Session["UsuarioSession"].ToString().ToUpper();
+            //        lbl_NombreUsuario.Text = "Bienvenido " + NombreUsuario;
 
-                    //Opciones Perfil de usuario
-                    this.opcNombreUsuario.Visible = true;
-                    this.opcNombreUsuario_Perfil.Visible = false;
-                    this.opcNombreUsuario_Payment.Visible = false;
-                    this.opcNombreUsuario_PerfilAdministrativo.Visible = true;
+            //        //Opciones Perfil de usuario
+            //        this.opcNombreUsuario.Visible = true;
+            //        this.opcNombreUsuario_Perfil.Visible = false;
+            //        this.opcNombreUsuario_Payment.Visible = false;
+            //        this.opcNombreUsuario_PerfilAdministrativo.Visible = true;
 
-                    this.opcIndex.Visible = true;
-                    this.opcDepartamentos.Visible = true;
+            //        this.opcIndex.Visible = true;
+            //        this.opcDepartamentos.Visible = true;
 
-                    this.opcAdministrativa.Visible = true;
-                    this.opcAdministrativa_Clientes.Visible = true;
-                    this.opcAdministrativa_Usuarios.Visible = false;
-                    this.opcAdministrativa_Productos.Visible = true;
+            //        this.opcAdministrativa.Visible = true;
+            //        this.opcAdministrativa_Clientes.Visible = true;
+            //        this.opcAdministrativa_Usuarios.Visible = false;
+            //        this.opcAdministrativa_Productos.Visible = true;
 
-                    this.opcCarrito.Visible = true;
-                    this.opcLogin.Visible = false;
-                    this.opcCerrarSesion.Visible = true;
-                }
-            }
+            //        this.opcCarrito.Visible = true;
+            //        this.opcLogin.Visible = false;
+            //        this.opcCerrarSesion.Visible = true;
+            //    }
+            //}
         }
         #endregion
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            this.AdministracionPerfiles();
+            //this.AdministracionPerfiles();
         }
 
         protected void btnCerrarSesion_Click(object sender, EventArgs e)
         {
-            Session.RemoveAll();
-            Response.Redirect("~/www/Index.aspx");
+            //Session.RemoveAll();
+            //Response.Redirect("~/www/Index.aspx");
         }
     }
 }
