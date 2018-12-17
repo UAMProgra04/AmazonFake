6e22e6ce5a984dec2c9fca36eeec6b7a014cf759
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2018-12-17 08:45:35 -0600

    Update_17122018_0845
    Se crea esqueleto para consulta de usuarios(clientes) en el proyecto web

--- Codigo/AmazonFake_WEB/AmazonFake_SITE/www/Administrador/Adm_Clientes.aspx
+++ Codigo/AmazonFake_WEB/AmazonFake_SITE/www/Administrador/Adm_Clientes.aspx
@@ -4,21 +4,26 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div>
         <center>
+            <asp:Label ID="Label1" runat="server" Text="Buscar Cliente por : "></asp:Label>
+            <asp:DropDownList ID="SeleccionBusqueda" runat="server">
+                <asp:ListItem Value="1">Correo</asp:ListItem>
+                <asp:ListItem Value="2">Nombre</asp:ListItem>
+                <asp:ListItem Value="3">Identificacion</asp:ListItem>
+            </asp:DropDownList>
+            <br />
             <asp:Label ID="lbl_Error" runat="server"></asp:Label>
             <br />
-            <asp:Label ID="Label1" runat="server" Text="Buscar Cliente: "></asp:Label>
-            <br />
             <asp:TextBox ID="txt_Buscar" runat="server" OnTextChanged="txt_Buscar_TextChanged"></asp:TextBox>
-            <br />
-            <asp:Label ID="lbl_Clientes" runat="server" Text="Lista de Clientes"></asp:Label>
-            <br />
-            <asp:GridView ID="GV_ALL_CLIENT" runat="server" ></asp:GridView>
+            <asp:Button ID="btn_CargarClientes" runat="server" Text="Buscar" onclick="btn_CargarClientes_Click"/>
         </center>
     </div>
     <div>
         <br />
         <center>
-            <asp:Button ID="btn_CargarClientes" runat="server" Text="Ver Clientes" onclick="btn_CargarClientes_Click"/>
+            
+            <asp:Label ID="lbl_Clientes" runat="server" Text="Lista de Clientes"></asp:Label>
+            <br />
+            <asp:GridView ID="GV_ALL_CLIENT" runat="server" ></asp:GridView>
         </center>
     </div>
 </asp:Content>
--- Codigo/AmazonFake_WEB/AmazonFake_SITE/www/Administrador/Adm_Clientes.aspx.cs
+++ Codigo/AmazonFake_WEB/AmazonFake_SITE/www/Administrador/Adm_Clientes.aspx.cs
@@ -29,15 +29,6 @@ namespace AmazonFake_SITE.www.Administrador
 
         protected void CargarListaClintes()
         {
-            //AmazonFakeService.I_Base_DatosClient cliente = new AmazonFakeService.I_Base_DatosClient();
-            /*I_Base_DatosClient cliente = new I_Base_DatosClient();
-
-            DataTable DataTableClient = cliente.Mostrar_Informacion("SP_View_All_Users", "USUARIOS");
-
-            GV_ALL_CLIENT.DataSource = null;
-            GV_ALL_CLIENT.DataSource = DataTableClient;
-            GV_ALL_CLIENT.DataBind();*/
-
             I_Base_DatosClient cliente = new I_Base_DatosClient();
             string SMsError = string.Empty;
 
@@ -57,7 +48,61 @@ namespace AmazonFake_SITE.www.Administrador
 
         protected void btn_CargarClientes_Click(object sender, EventArgs e)
         {
-            this.CargarListaClintes();
+            I_Base_DatosClient cliente = new I_Base_DatosClient();
+            DataTable myDataTable = new DataTable();
+            myDataTable.Columns.Add("Correo Electronico");
+            myDataTable.Columns.Add("Nombre del Cliente");
+            myDataTable.Columns.Add("Numero de Identificacion");
+            myDataTable.Columns.Add("Direccion");
+            myDataTable.Columns.Add("Telefono");
+            myDataTable.Columns.Add("Tipo de Perfil");
+
+            if (this.SeleccionBusqueda.SelectedValue == "1")
+            {
+                foreach (var item in cliente.View_Email_Users(txt_Buscar.Text))
+                {
+                    DataRow Fila = myDataTable.NewRow();
+                    Fila["Correo Electronico"] = item.SCorreo;
+                    Fila["Nombre del Cliente"] = item.SNombre;
+                    Fila["Numero de Identificacion"] = item.SIdentificacion;
+                    Fila["Direccion"] = item.SDireccion;
+                    Fila["Telefono"] = item.STelefono;
+                    Fila["Tipo de Perfil"] = item.SNombrePerfil;
+                    myDataTable.Rows.Add();
+                }
+            }
+            else if(this.SeleccionBusqueda.SelectedValue == "2"){
+                
+                foreach (var item in cliente.View_Name_Users(txt_Buscar.Text))
+                {
+                    DataRow Fila = myDataTable.NewRow();
+                    Fila["Correo Electronico"] = item.SCorreo;
+                    Fila["Nombre del Cliente"] = item.SNombre;
+                    Fila["Numero de Identificacion"] = item.SIdentificacion;
+                    Fila["Direccion"] = item.SDireccion;
+                    Fila["Telefono"] = item.STelefono;
+                    Fila["Tipo de Perfil"] = item.SNombrePerfil;
+                    myDataTable.Rows.Add();
+                }
+            }
+            else if(this.SeleccionBusqueda.SelectedValue == "3")
+            {
+                foreach (var item in cliente.View_ID_Users(txt_Buscar.Text))
+                {
+                    DataRow Fila = myDataTable.NewRow();
+                    Fila["Correo Electronico"] = item.SCorreo;
+                    Fila["Nombre del Cliente"] = item.SNombre;
+                    Fila["Numero de Identificacion"] = item.SIdentificacion;
+                    Fila["Direccion"] = item.SDireccion;
+                    Fila["Telefono"] = item.STelefono;
+                    Fila["Tipo de Perfil"] = item.SNombrePerfil;
+                    myDataTable.Rows.Add();
+                }
+            }
+
+            this.GV_ALL_CLIENT.DataSource = myDataTable;
+            this.GV_ALL_CLIENT.DataBind();
+
         }
 
         protected void txt_Buscar_TextChanged(object sender, EventArgs e)
@@ -65,8 +110,6 @@ namespace AmazonFake_SITE.www.Administrador
             I_Base_DatosClient cliente = new I_Base_DatosClient();
             DataTable myDataTable = new DataTable();
             string SMsError = string.Empty;
-
-
             //DataTable myDataTable = cliente.ListarDatos("SP_Filtrar", ref SMsError);
 
             if (txt_Buscar.Text.Trim() != string.Empty)
